f2f02298249280e070e5c2b313c65d658e155218
Ilya <desantt@mail.ru>
2023-02-10 15:10:33 +0600

    add Example005

---
+++ Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "ilya")
+{
+    Console.WriteLine("Big dick");
+}
+else
+{
+    Console.Write("no big dick");
+    Console.WriteLine("username");
+}
